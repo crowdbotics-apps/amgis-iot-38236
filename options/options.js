044a9cb40bbeaaf5e9750408b27ff4cf1ed13302
Crowdbotics <team@crowdbotics.com>
2023-01-09 22:31:25 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "amgis-iot-38236",
+  url: "https://amgis-iot-38236.botics.co",
+  api: "https://amgis-iot-38236.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
